9e55159dc336f61a904ea7a942ff1ec0104e8662
Michael Glasser <michaelglasser@Michaels-MacBook-Pro.local>
2019-07-25 22:29:00 -0400

    added input validation

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,6 +1,6 @@
 package src;
-import src.Board;
 
+import src.Board;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -8,15 +8,28 @@ public class TicTacToe {
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         Board board = new Board();
+        String space;
+        boolean validInput = false;
 
         System.out.print("Welcome to Tic-Tac-Toe, are you ready to play? Here we go!\n\n");
 
         do {
             board.switchTurns();
-            System.out.println(board.board);
-            System.out.print("Player " + board.playerTurn + ", what space do you want to play? : ");
-            int spaceNum = kb.nextInt();
-            board.markSpace(spaceNum);
+            
+            do {
+                System.out.println(board.board);
+                System.out.print("\nPlayer " + board.playerTurn + ", what space do you want to play? : ");
+                space = kb.nextLine();
+                if (board.spaces.contains(space)) {
+                    System.out.println();
+                    validInput = true;
+                } else {
+                    System.out.println("\nInvalid input. Please select an available space from 1-9\n");
+                    validInput = false;
+                }
+            } while (!validInput);
+            
+            board.markSpace(Integer.parseInt(space));
             board.didPlayerWin();
          } while ( !board.gameOver );
 
