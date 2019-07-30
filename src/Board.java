479f83a2b6bee258f1d9637fe0d72837ff1c987d
Michael Glasser <michaelglasser@Michaels-MBP.fios-router.home>
2019-07-29 22:18:51 -0400

    added helper methods, updated some methods, added docs

--- src/Board.java
+++ src/Board.java
@@ -3,62 +3,193 @@ package src;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/**
+ * The Board class contains the state of a
+ * game of TicTacToe and helper methods to
+ * update the board and check for game over.
+ * 
+ * @author Michael Glasser
+ * @since 2019-7-29
+ */
 public class Board {
-  public String board;
-  public int playerTurn;
-  public ArrayList<String> spaces;
-  public Boolean gameOver;
-  private int[][] winningLines = new int[][] { {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6} };
+  private int playerTurn;
+  private ArrayList<String> spaces;
+  private int[][] winningLines;
+  private String board;
 
+  /**
+   * Constructor for the Board class.
+   * Initalizes the board status for use
+   * in a TicTacToe game.
+   */
   public Board() {
-    gameOver = false;
-    playerTurn = 2;
-    spaces = new ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9")) ;
-    updateBoard();
+    setPlayerTurn(2);
+    spaces = new ArrayList<String>(Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9"));
+    winningLines = new int[][] { {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6} };
+    setBoard();
   }
 
-  public void switchTurns() {
-    if (playerTurn == 1) {
-      playerTurn = 2;
-    } else {
-      playerTurn = 1;
-    }
+  /**
+   * This method returns the playerTurn property
+   * so that the game can read the value.
+   * 
+   * @return int Returns the playerTurn property
+   */
+  public int getPlayerTurn() {
+    return playerTurn;
   }
 
-  public void markSpace(int space) {
-    if (playerTurn == 1) {
-      spaces.set(space - 1, "X");
-    } else {
-      spaces.set(space - 1, "O");
-    }
-    updateBoard();
+  /**
+   * This method sets the playerTurn property
+   * 
+   * @param turnNum The number that playerturn will be set to
+   */
+  private void setPlayerTurn(int turnNum) {
+    playerTurn = turnNum;
   }
 
-  private void updateBoard() {
+  /**
+   * This method returns the value in the spaces
+   * property at the defined index.
+   * 
+   * @param index The index to retrieve a value from
+   * @return String The value in the spaces property at the index
+   */
+  public String getSpace(int index) {
+    return spaces.get(index);
+  }
+
+  /**
+   * This method sets a value at index in the
+   * spaces property.
+   * 
+   * @param index The index in spaces that you want to update
+   * @param value The value to update to
+   */
+  private void setSpace(int index, String value) {
+    spaces.set(index - 1, value);
+  }
+
+  /**
+   * This method returns an int[] listing a possible
+   * winning line for Tic-Tac-Toe.
+   * 
+   * @param index The index of the int[] to return
+   * @return int[] Returns an int[] listing a possible winning line
+   */
+  private int[] getWinningLine(int index) {
+    return winningLines[index];
+  }
+
+  /**
+   * This method returns the board property so
+   * that the game can read the value.
+   * 
+   * @return String Returns the board property
+   */
+  public String getBoard() {
+    return board;
+  }
+
+  /**
+   * This method updates the board property to
+   * reflect any new changes from the players turn.
+   */
+  private void setBoard() {
     board = String.format(
       "%s|%s|%s\n" + "-----\n" + "%s|%s|%s\n" + "-----\n" + "%s|%s|%s", 
       spaces.get(0), spaces.get(1), spaces.get(2), spaces.get(3), spaces.get(4), spaces.get(5), spaces.get(6), spaces.get(7), spaces.get(8));
   }
 
-  public void didPlayerWin() {
+  /**
+   * This method switches the playerTurn
+   * between 1 and 2.
+   */
+  public void switchTurns() {
+    if (getPlayerTurn() == 1) {
+      setPlayerTurn(2);
+    } else {
+      setPlayerTurn(1);
+    }
+  }
+
+  /**
+   * This method checks the spaces property to
+   * see if it contains a given value.
+   * 
+   * @param value The value to check for
+   * @return boolean Returns true if value is found, false if not
+   */
+  public boolean doesSpacesContain(String value) {
+    for (int i = 0; i < spaces.size(); i++) {
+      if (getSpace(i).equals(value)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * This method assigns an X or O
+   * in the space defined by the player
+   * then updates the board.
+   * 
+   * @param space The number of the space provided by the player
+   */
+  public void markSpace(int space) {
+    if (getPlayerTurn() == 1) {
+      setSpace(space - 1, "X");
+    } else {
+      setSpace(space - 1, "O");
+    }
+    setBoard();
+  }
+
+  /**
+   * This method checks if the game board has
+   * resulted in a tie and sets the gameTied
+   * property accordingly.
+   * 
+   * @return boolean Returns true or false depending on if the game is in a tie state.
+   */
+  public boolean didGameTie() {
+    for (int i = 0; i < spaces.size(); i++) {
+      if (!getSpace(i).equals("X") && !getSpace(i).equals("O")) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * This method checks if the game board has
+   * resulted in a win and sets the gameWon
+   * property accordingly.
+   * 
+   * @return boolean Returns true or false depending on if the game is in a won state.
+   */
+  public boolean didPlayerWin() {
+    // Determine which letter to check for based on playerTurn
     String letterToCheck;
-    if (playerTurn == 1) {
+    if (getPlayerTurn() == 1) {
       letterToCheck = "X";
     } else {
       letterToCheck = "O";
     }
 
+    // Check all possible win conditions and return true if game is won or false if not
     for (int i = 0; i < winningLines.length; i++) {
       int count = 0;
-      for (int j = 0; j < winningLines[i].length; j++) {
-        if ( spaces.get(winningLines[i][j]).equals(letterToCheck) ) {
+      for (int j = 0; j < getWinningLine(i).length; j++) {
+        if ( getSpace(getWinningLine(i)[j]).equals(letterToCheck) ) {
           count++;
         }
       }
       if ( count == 3 ) {
-        gameOver = true;
-        return;
+        return true;
       }
     }
+
+    return false;
   }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -3,37 +3,56 @@ package src;
 import src.Board;
 import java.util.Scanner;
 
+/**
+ * The TicTacToe class contains a main method
+ * which is used to run a game of Tic-Tac-Toe.
+ * Each time the main method is run, a new instance
+ * of a Board is created to track the state of the
+ * game. A message is printed depending on if a player
+ * won or the game tied.
+ * 
+ * @author Michael Glasser
+ * @since 2019-7-29
+ */
 public class TicTacToe {
 
     public static void main(String[] args) {
+        // Initalize the game
         Scanner kb = new Scanner(System.in);
         Board board = new Board();
-        String space;
-        boolean validInput = false;
 
         System.out.print("Welcome to Tic-Tac-Toe, are you ready to play? Here we go!\n\n");
 
+        // Game loops until game over status is reached
         do {
+            // Switch turns
             board.switchTurns();
             
+            // Loop until a valid input is provided
+            String space;
+            boolean validInput = false;
             do {
-                System.out.println(board.board);
-                System.out.print("\nPlayer " + board.playerTurn + ", what space do you want to play? : ");
+                System.out.println(board.getBoard());
+                System.out.print("\nPlayer " + board.getPlayerTurn() + ", what space do you want to play? : ");
                 space = kb.nextLine();
-                if (board.spaces.contains(space)) {
+                if (board.doesSpacesContain(space)) {
                     System.out.println();
                     validInput = true;
                 } else {
                     System.out.println("\nInvalid input. Please select an available space from 1-9\n");
-                    validInput = false;
                 }
             } while (!validInput);
             
+            // Mark the space provided
             board.markSpace(Integer.parseInt(space));
-            board.didPlayerWin();
-         } while ( !board.gameOver );
+         } while ( !board.didGameTie() && !board.didPlayerWin() );
 
-         System.out.println("Game is over now. Player " + board.playerTurn + " is the winner!");
+         // Print the appropriate game over message
+         if (board.didPlayerWin()) {
+            System.out.println("Game is over now. Player " + board.getPlayerTurn() + " is the winner!");
+         } else if (board.didGameTie()) {
+            System.out.println("Game is over now. There was a tie! Play again to determine the winner!");
+         }
 
         kb.close();
     }
